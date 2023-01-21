1b3f3747e698c1cd16c2da206cce94405ed9b664
markenders <mark.enders.wi@gmail.com>
2023-01-20 22:54:44 -0600

    Updated main and actions files. Added credentials

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
 from datetime import date
 from Sendemail import send_email
+from dotenv import load_dotenv
 import gspread
+import os
 
-# open google account and sheet title/worksheet
-sa = gspread.service_account()
+# load env vars
+load_dotenv()
+
+# format with .replace
+pkey = os.getenv('private_key').replace('\\n', '\n')
+
+credentials = {
+    "type": "service_account",
+    "project_id": os.getenv('project_id'),
+    "private_key_id": os.getenv('private_key_id'),
+    "private_key": pkey,
+    "client_email": os.getenv('client_email'),
+    "client_id": os.getenv('client_id'),
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": os.getenv('client_x509_cert_url')
+}
+
+sa = gspread.service_account_from_dict(credentials)
 sh = sa.open("DailyEmailPythonResponses")
 wks = sh.worksheet("Sheet1")
 
