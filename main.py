53459835bbe47a800ba5e527b7933bb3f47231e7
markenders <mark.enders.wi@gmail.com>
2023-01-22 22:06:11 -0600

    Uncommented some code, updated actions.yml

--- main.py
+++ main.py
@@ -65,21 +65,21 @@ if (len(u_list) != 1):
     print("Results of unsubscribe (Email_Unsub is supposed to be there): " + str(u_list))
 
 
-# print('Emails to be sent: ', wks.row_count-1)
+print('Emails to be sent: ', wks.row_count-1)
 
-# # last row to go to
-# maxrow = wks.row_count
-# # first row to start on
-# row = 2
-# # tracks emails sent
-# sentEmails = 0
+# last row to go to
+maxrow = wks.row_count
+# first row to start on
+row = 2
+# tracks emails sent
+sentEmails = 0
 
-# while row <= maxrow:
-#     send_email(
-#         Name=wks.acell('B'+str(row)).value,
-#         email_receiver=wks.acell('C'+str(row)).value
-#     )
-#     row += 1
-#     sentEmails += 1
+while row <= maxrow:
+    send_email(
+        Name=wks.acell('B'+str(row)).value,
+        email_receiver=wks.acell('C'+str(row)).value
+    )
+    row += 1
+    sentEmails += 1
 
-# print('Total emails sent: ' + str(sentEmails))
+print('Total emails sent: ' + str(sentEmails))
